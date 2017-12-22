bf80da3924a587dffd8be2ba1faf25e086c05fbe
Michał Kucharski <m-kucharski@wp.pl>
2017-12-22 22:59:51 +0100

    Change width of board for hard difficutly ( 8x4 )

--- scripts/script.js
+++ scripts/script.js
@@ -16,6 +16,7 @@ $(function () {
         var $tile = $('.tile');
         for (i = 0; i < 32; i++)
             $("#board").append('<div id="c' + ($tile.length + i) + '" class="card"></div>');
+            $("#board").css("width", "1200px");
     });
 });
 
@@ -45,109 +46,109 @@ shuffle(cardsEasy);
 shuffle(cardsHard);
 
 
-$(function() {
-$('#easy').on('click', function() {
+$(function () {
+    $('#easy').on('click', function () {
 
-c0.addEventListener("click", function () {
-revealCard(0);
+        c0.addEventListener("click", function () {
+            revealCard(0);
         });
         c1.addEventListener("click", function () {
-        revealCard(1);
-                });
+            revealCard(1);
+        });
         c2.addEventListener("click", function () {
-        revealCard(2);
-                });
+            revealCard(2);
+        });
         c3.addEventListener("click", function () {
-        revealCard(3);
-                });
+            revealCard(3);
+        });
         c4.addEventListener("click", function () {
-        revealCard(4);
-                });
+            revealCard(4);
+        });
         c5.addEventListener("click", function () {
-        revealCard(5);
-                });
+            revealCard(5);
+        });
         c6.addEventListener("click", function () {
-        revealCard(6);
-                });
+            revealCard(6);
+        });
         c7.addEventListener("click", function () {
-        revealCard(7);
-                });
+            revealCard(7);
+        });
         c8.addEventListener("click", function () {
-        revealCard(8);
-                });
+            revealCard(8);
+        });
         c9.addEventListener("click", function () {
-        revealCard(9);
-                });
+            revealCard(9);
+        });
         c10.addEventListener("click", function () {
-        revealCard(10);
-                });
+            revealCard(10);
+        });
         c11.addEventListener("click", function () {
-        revealCard(11);
-                });
+            revealCard(11);
+        });
         c12.addEventListener("click", function () {
-        revealCard(12);
-                });
+            revealCard(12);
+        });
         c13.addEventListener("click", function () {
-        revealCard(13);
-                });
+            revealCard(13);
+        });
         c14.addEventListener("click", function () {
-        revealCard(14);
-                });
+            revealCard(14);
+        });
         c15.addEventListener("click", function () {
-        revealCard(15);
-                });
-                  });
+            revealCard(15);
+        });
+    });
 });
-        var oneVisible = false;
-        var turnCounter = 0;
-        var visible_nr;
-        var lock = false;
-        var pairsLeft = 8;
-        function revealCard(nr) {
+var oneVisible = false;
+var turnCounter = 0;
+var visible_nr;
+var lock = false;
+var pairsLeft = 8;
+function revealCard(nr) {
 
-        var opacityValue = $('#c' + nr + 1).css('opacity');
-                if (opacityValue != 0 && lock == false) {
+    var opacityValue = $('#c' + nr + 1).css('opacity');
+    if (opacityValue != 0 && lock == false) {
         lock = true;
-                var image = "url(img/easier/" + cardsEasy[nr] + ")";
-                $('#c' + nr).css('background-image', image);
-                $('#c' + nr).toggleClass('cardA');
-                if (oneVisible == false)
+        var image = "url(img/easier/" + cardsEasy[nr] + ")";
+        $('#c' + nr).css('background-image', image);
+        $('#c' + nr).toggleClass('cardA');
+        if (oneVisible == false)
         {
-        oneVisible = true;
-                visible_nr = nr;
-                lock = false;
+            oneVisible = true;
+            visible_nr = nr;
+            lock = false;
         } else
         {
-        if (cardsEasy [visible_nr] == cardsEasy[nr]) {
-        setTimeout(function () {
-        hide2Cards(nr, visible_nr)
-        }, 750);
-        } else {
-        setTimeout(function () {
-        restore2Cards(nr, visible_nr)
-        }, 1000);
-        }
+            if (cardsEasy [visible_nr] == cardsEasy[nr]) {
+                setTimeout(function () {
+                    hide2Cards(nr, visible_nr)
+                }, 750);
+            } else {
+                setTimeout(function () {
+                    restore2Cards(nr, visible_nr)
+                }, 1000);
+            }
 
-        turnCounter++;
-                $('.score').html('Licznik rund : ' + turnCounter);
-                oneVisible = false;
+            turnCounter++;
+            $('.score').html('Licznik rund : ' + turnCounter);
+            oneVisible = false;
         }
-        }
-        }
-function hide2Cards(nr1, nr2) {
-$('#c' + nr1).css('opacity', '0');
-        $('#c' + nr2).css('opacity', '0');
-        pairsLeft--;
-        if (pairsLeft == 0) {
-$('.board').html('<h1>Wygrałeś!<br>Ukończyłeś grę w ' + turnCounter + ' tur.</h1>');
+    }
 }
+function hide2Cards(nr1, nr2) {
+    $('#c' + nr1).css('opacity', '0');
+    $('#c' + nr2).css('opacity', '0');
+    pairsLeft--;
+    if (pairsLeft == 0) {
+        $('.board').html('<h1>Wygrałeś!<br>Ukończyłeś grę w ' + turnCounter + ' tur.</h1>');
+    }
 
-lock = false;
-        }
+    lock = false;
+}
 function restore2Cards(nr1, nr2) {
-$('#c' + nr1).css('background-image', 'url(img/easier/ct.png)');
-        $('#c' + nr1).toggleClass('cardA');
-        $('#c' + nr2).css('background-image', 'url(img/easier/ct.png)');
-        $('#c' + nr2).toggleClass('cardA');
-        lock = false;
-        }
+    $('#c' + nr1).css('background-image', 'url(img/easier/ct.png)');
+    $('#c' + nr1).toggleClass('cardA');
+    $('#c' + nr2).css('background-image', 'url(img/easier/ct.png)');
+    $('#c' + nr2).toggleClass('cardA');
+    lock = false;
+}
